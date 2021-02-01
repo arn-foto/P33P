025d0bac77b86090ca8bd7650be946f646fd0cdd
Anthony N <foto.arn@gmail.com>
2021-02-01 00:49:17 -0800

    + Working on AboutMe page

--- src/Js/AboutMe.js
+++ src/Js/AboutMe.js
@@ -18,13 +18,18 @@ function AboutMe() {
 			<div className="aboutMe__git">
 				<Git />
 			</div>
-
-			<div className="aboutMe__bottom">
-				I am a Front-end developer with an obsession for all things tech. As
-				much as I consider myself a software engineer, I mostly assume the role
-				of student on a daily basis, constantly searching and learning new
-				technologies and ways to apply them to everything I do.
+			<div className="aboutMe__archive">
+				<h4>Archived Websites</h4>
+				<a
+					target="_blank"
+					rel="noopener noreferrer"
+					href="https://dev-ant.netlify.app/"
+				>
+					First site
+				</a>
 			</div>
+
+			<div className="aboutMe__bottom"></div>
 		</div>
 	);
 }
--- src/Js/App.js
+++ src/Js/App.js
@@ -12,7 +12,6 @@ function App() {
 		<Router>
 			<div className="app">
 				<Header />
-
 				<div className="app__body">
 					<Switch>
 						<Route path="/about">
