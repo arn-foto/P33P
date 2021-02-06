8cd28f45c9424c59ed8b5e2816ca94e7f46a4326
Anthony N <foto.arn@gmail.com>
2021-02-06 12:39:13 -0800

    + aboutMe__middle

--- src/Js/AboutMe.js
+++ src/Js/AboutMe.js
@@ -9,14 +9,15 @@ function AboutMe() {
 		<div className="aboutMe">
 			<img src={Stack} width="30%" />
 			<div className="aboutMe__top">
-				I am a Front-end developer with an obsession for all things tech. As
-				much as I consider myself a software engineer, I mostly assume the role
-				of student on a daily basis, constantly searching and learning new
-				technologies and ways to apply them to everything I do.
+				I am a nerd with an obsession for all things tech. As much as I consider
+				myself a software engineer, I mostly assume the role of student on a
+				daily basis, constantly searching and learning new technologies and ways
+				to apply them to everything I do.
 			</div>
 
 			<br />
 
+			<div className="aboutMe__middle">I like chips</div>
 			<br />
 
 			<div className="aboutMe__bottom">
