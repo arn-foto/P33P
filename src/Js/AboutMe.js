4aaa3607b8b8e2f104ef44529a5e662fea2c7461
Anthony N <foto.arn@gmail.com>
2021-02-07 21:32:07 -0800

    + Forever fighting with the about me page

--- src/Js/AboutMe.js
+++ src/Js/AboutMe.js
@@ -9,31 +9,35 @@ function AboutMe() {
 		<div className="aboutMe">
 			<img src={Stack} width="30%" />
 			<div className="aboutMe__top">
-				I am a nerd with an obsession for all things tech. As much as I consider
-				myself a software engineer, I mostly assume the role of student on a
-				daily basis, constantly searching and learning new technologies and ways
-				to apply them to everything I do.
+				What can I say...I'm a nerd with an obsession for all things tech. As
+				much as I consider myself a software engineer, I mostly assume the role
+				of student on a regualr basis, constantly searching for new technology
+				and ways to learn it, break it, & fix it. Then apply it to everything I
+				do.
 			</div>
 
 			<br />
 
-			<div className="aboutMe__middle">I like chips</div>
+			<div className="aboutMe__middle">
+				<div className="aboutMe__git">
+					<Git />
+					<Card>
+						<div className="aboutMe__archive">
+							<h4>Archived Portfolio's</h4>
+							<a
+								target="_blank"
+								rel="noopener noreferrer"
+								href="https://dev-ant.netlify.app/"
+							>
+								Site #1
+							</a>
+						</div>
+					</Card>
+				</div>
+			</div>
 			<br />
 
-			<div className="aboutMe__bottom">
-				<Card>
-					<div className="aboutMe__archive">
-						<h4>Archived Websites</h4>
-						<a
-							target="_blank"
-							rel="noopener noreferrer"
-							href="https://dev-ant.netlify.app/"
-						>
-							First site
-						</a>
-					</div>
-				</Card>
-			</div>
+			<div className="aboutMe__bottom"></div>
 		</div>
 	);
 }
