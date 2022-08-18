1e95c6468bb0181cc86aee31342634cfb61cecfb
Anthony-Navarro <foto.arn@gmail.com>
2022-08-17 23:02:40 -0700

    Change about me

--- src/Js/AboutMe.js
+++ src/Js/AboutMe.js
@@ -5,14 +5,11 @@ import Stack from "../img/fullstack.png";
 
 function AboutMe() {
 	return (
+
 		<div className="aboutMe">
 			<img src={Stack} width="30%" />
 			<div className="aboutMe__top">
-				What can I say...I'm a nerd with an obsession for all things tech. As
-				much as I consider myself a software engineer, I mostly assume the role
-				of student on a regualr basis, constantly searching for new technology
-				and ways to learn it, break it, & fix it. Then apply it to everything I
-				do.
+				I'm a nerd.
 			</div>
 			<br></br>
 			<Git />
@@ -20,7 +17,7 @@ function AboutMe() {
 				JavaScript | React | Redux | Java | SpringBoot | PostgreSQL | MySQL |
 				RESTful API | Python | HTML | CSS | Git | Agile
 			</div>
-
+			
 			<div className="aboutMe__bottom">
 				<div className="aboutMe__archive">
 					<h4>Archived Portfolio(s)</h4>
